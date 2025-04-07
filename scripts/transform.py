6e149cb3ea9c1fe4931cb30fff30bef44b6539ac
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-04-07 11:59:41 -0300

    define transfor_data function

---
+++ scripts/transform.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def transform_data(raw_data):
+    """
+    Transforma los datos crudos del Titanic para su análisis.
+
+    Args:
+        raw_data (pd.DataFrame): Datos crudos del Titanic.
+
+    Returns:
+        clean_data (pd.DataFrame): Datos transformados.
+    """
