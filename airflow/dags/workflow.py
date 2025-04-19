494699a5d95fd661ebcbcfb86a839bd65e0ab089
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-04-19 18:38:09 -0300

    found better way to pass arg to functions on tasks

--- airflow/dags/workflow.py
+++ airflow/dags/workflow.py
@@ -3,6 +3,9 @@ import os
 
 import logging
 
+root_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root_path)
+
 from scripts.extract import extract_data
 from scripts.transform import transform_data
 from scripts.load import load_data
@@ -11,10 +14,6 @@ from airflow.models.dag import DAG
 from airflow.operators.python import PythonOperator
 
 from datetime import datetime, timedelta
-
-
-root_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(root_path)
 
 
 def setup_logger(log_file):
@@ -38,28 +37,33 @@ with DAG(
         'start_date': datetime(2023, 10, 1),
         'email_on_failure': False,
         'email_on_retry': False,
-        'retries': 1,
+        'retries': 3,
         'retry_delay': timedelta(minutes=5),
     },
+    schedule_interval='@daily',
 ) as dag:
-    log_file = '../data/pipeline.log'
+    log_file = '/home/gabriel/repos/etl_airflow/data/pipeline.log'
     logger = setup_logger(log_file)
 
     extract_task = PythonOperator(
         task_id='extract',
         python_callable=extract_data,
-        op_kwargs={'file_path': '../data/raw_titanic_data.csv',
+        op_kwargs={'file_path': '/home/gabriel/repos/etl_airflow/data/raw_titanic_data.csv',
                    'logger': logger},
     )
+
     transform_task = PythonOperator(
         task_id='transform',
         python_callable=transform_data,
-        op_kwargs={'raw_data': '{{ task_instance.xcom_pull(task_ids="extract") }}'},
+        op_kwargs={'raw_data': extract_task.output,
+                   'logger': logger},
     )
+
     load_task = PythonOperator(
         task_id='load',
         python_callable=load_data,
-        op_kwargs={'clean_df': '{{ task_instance.xcom_pull(task_ids="transform") }}'},
+        op_kwargs={'clean_df': transform_task.output,
+                   'logger': logger},
     )
 
     extract_task >> transform_task >> load_task
