5fb1ed910ee29addbd7d5de3c606885445795adc
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-04-19 12:54:36 -0300

    update load function to work with SQLite instead

---
+++ scripts/load.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import sqlite3
+
+dbname = 'titanic.db'
+table_name = 'titanic_data'
+conn = sqlite3.connect(dbname)
+
+def load_data(clean_df):
+    """
+    Carga los datos transformados en una base de datos SQLite.
+
+    Args:
+        clean_df (pd.DataFrame): Datos transformados.
+    """
+    try:
+        clean_df.to_sql(table_name, conn, if_exists='replace', index=False)
+    except Exception as e:
+        print(f"Se produjo un error al cargar los datos: {e}")
+    finally:
+        conn.close()
